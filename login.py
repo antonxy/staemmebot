5fbd6392c0a7bf0e4fc9d1c58e00985306f7a56f
Anton Schirg <anton.schirg@posteo.de>
2016-12-24 22:50:02 +0100

    upgrade buildings

--- login.py
+++ login.py
@@ -19,7 +19,7 @@ class Village(object):
         self.name = None
         self.resources = None
         self.production = None
-        self.troops = None
+        self.units = None
 
 def login(session, user):
     #Load main page to get cookies
@@ -64,6 +64,63 @@ def update_resources(session, user, village):
             'pop_max': resources_table.find("span", id="pop_max_label").text
             }.iteritems()}
 
+    production_table = soup.find("div", id="show_prod")
+    strongs = production_table.find_all("strong")
+    village.production = dict(zip(["wood", "stone", "iron"], map(lambda x: int(x.text), strongs)))
+
+    units_table = soup.find("div", id="show_units")
+    tds = units_table.find_all("td")
+    def parse_unit(td):
+        unit_link = td.find("a", class_="unit_link")
+        if unit_link:
+            unit_type = unit_link['data-unit']
+            unit_value = int(td.find("strong").text)
+            return (unit_type, unit_value)
+        return None
+    village.units = dict(filter(None, [parse_unit(td) for td in tds]))
+
+def get_buildings(session, user, village):
+    buildings_page_req = session.get(server_url.format(user.server) + "game.php?village={}&screen=main".format(village.id))
+    soup = BeautifulSoup(buildings_page_req.content, 'html.parser')
+    buildings_table = soup.find("table", id="buildings")
+    trs = buildings_table.find_all("tr")
+    def parse_row(tr):
+        if 'id' in tr.attrs:
+            res = {}
+            building_id = str(re.match("main_buildrow_(.*)", tr['id']).group(1))
+            first_td = tr.find("td")
+            level_text = first_td.find("span").text
+            level_match = re.match("Stufe ([0-9]*)", level_text)
+            level = 0
+            if level_match:
+                level = int(level_match.group(1))
+            res['level'] = level
+            
+            build_options = tr.find("td", class_="build_options")
+            fully_built = build_options is None
+            res['fully_built'] = fully_built
+            if not fully_built:
+                error_message = build_options.find("div", class_="inactive")
+                buildable = False
+                if error_message is None: 
+                    build_a = build_options.find("a", id="main_buildlink_" + building_id + "_" + str(level + 1))
+                    if build_a:
+                        buildable = True
+                        build_link = build_a['href']
+                        h_val = re.search("h=([0-9a-zA-Z]*)", build_link).group(1)
+                        res['h_val'] = h_val
+                res['buildable'] = buildable
+            else:
+                res['buildable'] = False
+
+            return (building_id, res)
+    return dict(filter(None, map(parse_row, trs)))
+
+def upgrade_building(session, user, village, building_id, h_val):
+    print("Upgrading {}".format(building_id))
+    building_upgrade_req = session.get(server_url.format(user.server) + "game.php?village={}&screen=main&action=upgrade_building&id={}&type=main&h={}".format(village.id, building_id, h_val))
+    print(building_upgrade_req)
+
 
 def main():
     user = User()
@@ -74,9 +131,17 @@ def main():
     s = requests.Session()
     login(s, user)
     village = get_villages(s, user)[0]
-    update_resources(s, user, village)
 
+    update_resources(s, user, village)
     print village.resources
+    print village.production
+    print village.units
+
+    buildings = get_buildings(s, user, village)
+    print buildings 
+    
+    if buildings['wood']['buildable']:
+        upgrade_building(s, user, village, 'wood', buildings['wood']['h_val'])
 
 if __name__ == "__main__":
     main()
